4bb3bba07475a12dc6ff7052fc23af28630e5b7e
Karl Lehenbauer <karl-gh@lehenbauer.com>
2013-08-28 10:29:36 -0500

    Update with a blurb and license.

--- Makefile
+++ Makefile
@@ -1,8 +1,8 @@
 
 all:	release/killersolos-pitchcaster.txt release/killersolos-humandoubler.txt
 
-release/killersolos-pitchcaster.txt:	learner-class.js synthetickeyboard.js pitchcaster.js
-	cat learner-class.js synthetickeyboard.js pitchcaster.js >release/killersolos-pitchcaster.txt
+release/killersolos-pitchcaster.txt:	pitchcaster-blurb.js license.js learner-class.js synthetickeyboard.js pitchcaster.js
+	cat pitchcaster-blurb.js license.js learner-class.js synthetickeyboard.js pitchcaster.js >release/killersolos-pitchcaster.txt
 
 
 release/killersolos-humandoubler.txt:	humandoubler.js
---
+++ pitchcaster-blurb.js
@@ -0,0 +1,20 @@
+/*
+
+Killer Solos Pitchcaster v1.0
+
+Beware MIDI loops.  Watch the video, YouTube user bitwonk.
+
+Copy this file by doing option-A, option-C in Text Editor.  
+
+Then in Logic add a MIDI effect to a mixer channel and select scripter.  
+
+Hit Save As... Killer Solos Pitchcaster.
+
+Then click on Open Script in Editor and in the editor, select all (option-A) and hit backspace to wipe out what's there, hit option-V to paste in the program, and off you go.
+
+Sorry this is clumsy but it's easier to deal with programs as text and the format Logic uses to save scripts into isn't text.
+
+Eventually there should be something better.
+
+*/
+
